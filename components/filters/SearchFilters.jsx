a17184eee125488e901f5d601c91db5331691b7f
meherab-hossain <h.meherabhossain@gmail.com>
2022-03-26 05:10:14 +0600

    search filter data and functionality added

--- components/filters/SearchFilters.jsx
+++ components/filters/SearchFilters.jsx
@@ -9,12 +9,41 @@ import {
   Icon,
   Button,
 } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import router, { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
-
+import { filterData, getFilterValues } from "../../utills/filterData";
 const SearchFilters = () => {
-  return <div>SearchFilters</div>;
+  const [filters, setFilters] = useState(filterData);
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+    const values = getFilterValues(filterValues);
+    values.forEach((item) => {
+      query[item.name] = item.value;
+    });
+    router.push({ pathname: path, query });
+  };
+  return (
+    <Flex bg="gray.100" p="4" flexWrap="wrap" justifyContent="center">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilters;
